6e74fb1e2acd9eda684006430e5d3d952d1b2c99
NavidMahbub <navidmahbub27@gmail.com>
2021-09-23 13:44:58 +0600

    storing images in local storage

--- src/acitons/FetchProductsAction.js
+++ src/acitons/FetchProductsAction.js
@@ -1,12 +1,21 @@
 import axios from "axios";
 import { FETCH_PRODUCTS } from "../actionsType/AcitonsTypes";
+import imageToBase64 from "image-to-base64/browser";
 
-export const fetchProducts = async(dispatch) => {
+export const fetchProducts = async (dispatch) => {
+    const res = await axios
+        .get("https://fakestoreapi.com/products")
+        .then((responses) => {
 
-    const res = await axios.get("https://fakestoreapi.com/products")
-    
-    dispatch({
-        type: FETCH_PRODUCTS,
-        payload: res.data
-    })
-}
+            for (let r of responses.data) {
+                imageToBase64(r.image).then((ress) =>
+                    localStorage.setItem(r.id, ress)
+                );
+            }
+            return responses.data;
+        })
+        dispatch({
+            type: FETCH_PRODUCTS,
+            payload: res,
+        });
+};
